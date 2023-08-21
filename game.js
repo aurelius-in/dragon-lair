81b30763327be81c72356bac45eba24b3bce6b3e
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-21 17:44:16 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 const canvas = document.getElementById('game');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 const context = canvas.getContext('2d');
 
 const dragon = {
-    x: 100,
-    y: 300,
+    x: canvas.width * 0.1,
+    y: canvas.height * 0.5,
     width: 50,
     height: 50,
     velocity: 0
@@ -24,6 +27,7 @@ function draw() {
     obstacles.forEach(obstacle => {
         context.fillStyle = 'green';
         context.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+        context.fillRect(obstacle.x, obstacle.y + obstacle.gap + obstacle.height, obstacle.width, canvas.height - (obstacle.y + obstacle.gap + obstacle.height));
     });
 }
 
@@ -40,20 +44,23 @@ function update() {
     // Generate new obstacles
     if (obstacles.length < level) {
         const height = Math.random() * 200 + 100; // Random height
-        obstacles.push({ x: canvas.width, y: canvas.height - height, width: 50, height: height });
+        const gap = 200; // Gap between top and bottom obstacles
+        obstacles.push({ x: canvas.width, y: 0, width: 50, height: height, gap: gap });
     }
 
     // Check collisions
     obstacles.forEach(obstacle => {
         if (
-            dragon.x < obstacle.x + obstacle.width &&
-            dragon.x + dragon.width > obstacle.x &&
-            dragon.y < obstacle.y + obstacle.height &&
-            dragon.y + dragon.height > obstacle.y
+            (dragon.x < obstacle.x + obstacle.width && dragon.x + dragon.width > obstacle.x) &&
+            ((dragon.y < obstacle.y + obstacle.height && dragon.y + dragon.height > obstacle.y) ||
+            (dragon.y < obstacle.y + obstacle.gap + obstacle.height * 2 && dragon.y + dragon.height > obstacle.y + obstacle.gap + obstacle.height))
         ) {
             // Collision detected
             console.log('Game Over, Bro!');
-            // TODO: Handle game over or restart level
+            obstacles.length = 0; // Clear obstacles
+            dragon.y = canvas.height * 0.5; // Reset dragon position
+            dragon.velocity = 0; // Reset dragon velocity
+            level = 1; // Reset level
         }
     });
 
@@ -62,6 +69,9 @@ function update() {
 
 // Event listener for player input
 window.addEventListener('click', () => {
+    dragon.velocity = -10; // Flap
+});
+window.addEventListener('touchstart', () => {
     dragon.velocity = -10; // Flap
 });
 
