76b47c8a362e9c58ed49dd616633bbd4b661c683
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-22 06:50:35 -0500

    Update game.js

--- game.js
+++ game.js
@@ -21,10 +21,11 @@ let currentFrame = 0; // Current frame being displayed
 const dragon = {
     x: canvas.width * 0.1,
     y: canvas.height * 0.5,
-    width: 50,
-    height: 50,
+    width: 180, // Updated width to match dragon image
+    height: 180, // Updated height to match dragon image
     velocity: 0
 };
+
 
 const obstacles = [];
 let gameTime = 0;
@@ -68,8 +69,8 @@ function update() {
     gameTime += deltaTime;
 
     // Update dragon's position
-    dragon.velocity += 1; // Gravity
-    dragon.y += dragon.velocity;
+    dragon.velocity += 0.5; // Reduced gravity
+    dragon.y += dragon.velocity * 0.5; // Slowed down the dragon
 
     // Animate dragon
     if (gameTime % 10 === 0) { // Change frame every 10 game ticks, or whatever interval you prefer
@@ -99,15 +100,16 @@ function update() {
         if (
             dragon.x < obstacle.x + obstacle.width &&
             dragon.x + dragon.width > obstacle.x &&
-            dragon.y < obstacle.y + obstacle.height &&
-            dragon.y + dragon.height > obstacle.y
+            (
+                (dragon.y < obstacle.y + obstacle.height && dragon.y + dragon.height > obstacle.y) ||
+                (dragon.y < obstacle.y + obstacle.height + gap + obstacle.height && dragon.y + dragon.height > obstacle.y + obstacle.height + gap)
+            )
         ) {
             // Collision detected
             console.log('Game Over, Bro!');
             obstacles.length = 0; // Clear obstacles
             dragon.y = canvas.height * 0.5; // Reset dragon position
             dragon.velocity = 0; // Reset dragon velocity
-            level = 1; // Reset level
             obstacleSpawnTime = 5000; // Reset obstacle spawn time to 5 seconds
             lastObstacleTime = now; // Reset last obstacle time
             gameTime = 0; // Reset game time
@@ -117,12 +119,13 @@ function update() {
     draw();
 }
 
+
 // Event listener for player input
 window.addEventListener('click', () => {
-    dragon.velocity = -10; // Flap
+    dragon.velocity = -5; // Reduced flap strength
 });
 window.addEventListener('touchstart', () => {
-    dragon.velocity = -10; // Flap
+    dragon.velocity = -5; // Reduced flap strength
 });
 
 // Game loop
