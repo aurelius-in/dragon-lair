ba98be69ef2fb0fb5f129ba0cc603352be382a37
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-25 10:48:28 -0500

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ import {
     createWraithObstacle, createZombieDragonObstacle, createThundercloudObstacle, createFireballObstacle
 } from './obstacles.js';
 
-let framesPerFlap = 100, gameLoopCounter = 0, gameStarted = false, obstacle, jump = 8;
+let framesPerFlap = 100, gameLoopCounter = 0, gameStarted = false, jump = 8;
 
 function handleInput() {
     if (!gameStarted) {
@@ -26,14 +26,6 @@ window.addEventListener('keydown', (e) => {
         handleInput();
 });
 
-window.addEventListener('click', handleInput);
-window.addEventListener('touchstart', handleInput);
-window.addEventListener('keydown', (e) => {
-    if (e.code === 'Space') {
-        handleInput();
-    }
-});
-
 function resetGame() {
     obstacles.length = 0;
     perch.x = 50;
@@ -41,7 +33,7 @@ function resetGame() {
     dragon.y = dragonStartY;
     dragon.velocity = 0;
     gameStarted = false;
-    .current = 0;
+    frame.current = 0; // Fixed this line
     backgrounds.bgX = 0;
     backgrounds.fgX = 0;
     backgrounds.bgbgX = 0;
@@ -52,143 +44,14 @@ function resetGame() {
     screenFade.alpha = 0;
     bg.width = canvas.height * 4;
 }
-function createObstacle() {
-    const obstacleType = [
-        'arrow',
-        'lightningStrike',
-        'batSwarm',
-        'tornado',
-        'wraith',
-        'zombieDragon',
-        'thundercloud',
-        'fireball'
-    ];
-    const randomType = obstacleType[Math.floor(Math.random() * obstacleType.length)];
 
-    const minDistance = canvas.height * 0.1;
-    const centerDistance = canvas.height * 0.5;
-
-    let obstacleY;
-    if (topObstacle) {
-        obstacleY = Math.random() * (centerDistance - minDistance) + minDistance;
-    } else {
-        obstacleY = Math.random() * (centerDistance - minDistance) + centerDistance;
-    }
-
-    let obstacle;
-    switch (randomType) {
-        case 'arrow': obstacle = createArrowObstacle(canvas.width, obstacleY);
-            break;
-        case 'lightningStrike': obstacle = createLightningStrikeObstacle(canvas.width, obstacleY);
-            break;
-        case 'batSwarm': obstacle = createBatSwarmObstacle(canvas.width, obstacleY);
-            break;
-        case 'tornado': obstacle = createTornadoObstacle(canvas.width, obstacleY);
-            break;
-        case 'wraith': obstacle = createWraithObstacle(canvas.width, obstacleY);
-            break;
-        case 'zombieDragon': obstacle = createZombieDragonObstacle(canvas.width, obstacleY);
-            break;
-        case 'thundercloud': obstacle = createThundercloudObstacle(canvas.width, obstacleY);
-            break;
-        case 'fireball': obstacle = createFireballObstacle(canvas.width, obstacleY);
-            break;
-    }
-
-    obstacles.push(obstacle);
-
-    topObstacle = !topObstacle;
-
-    obstacleSpawnTime *= 0.999;
-}
-
-function collisionDetected(dragon, obstacle) {
-    const boundaryReductionX = dragon.width * 0.05;
-    const boundaryReductionY = dragon.height * 0.1;
-
-    const dragonCollisionArea = {
-        x: dragon.x + boundaryReductionX,
-        y: dragon.y + boundaryReductionY,
-        width: dragon.width -(boundaryReductionX * 2),
-        height: dragon.height -(boundaryReductionY * 2)
-    };
-
-    const obstacleCollisionArea = {
-        x: obstacle.x,
-        y: obstacle.y,
-        width: obstacle.width,
-        height: obstacle.height
-    };
-
-    return(dragonCollisionArea.x<obstacleCollisionArea.x + obstacleCollisionArea.width &&
-    dragonCollisionArea.x + dragonCollisionArea.width>obstacleCollisionArea.x && dragonCollisionArea.y<obstacleCollisionArea.y + obstacleCollisionArea.height &&
-    dragonCollisionArea.y + dragonCollisionArea.height>obstacleCollisionArea.y);
-}
-function update() {
-    if (gameStarted) {
-        dragon.update();
-
-        obstacles.forEach((obstacle, index) => {
-            obstacle.update();
-            if (obstacle.x + obstacle.width < 0) {
-                obstacles.splice(index, 1);
-            }
-
-            if (collisionDetected(dragon, obstacle)) {
-                if (!dragon.collided) {
-                                        lifeBar.segments--;
-                    if (lifeBar.segments <= 0) {
-                        resetGame();
-                    }
-                    obstacles.splice(index, 1);
-                    dragon.collided = true;
-
-                    setTimeout(() => {
-                        dragon.collided = false;
-                    }, 1000);
-                }
-            }
-        });
-
-        perch.update();
-
-        if (obstacleSpawnTimer > obstacleSpawnRate) {
-            obstacles.push(new Obstacle());
-            obstacleSpawnTimer = 0;
-        } else {
-            obstacleSpawnTimer++;
-        }
-    }
-}
-
-if (backgrounds.fgX + bg.width <= canvas.width) {
-    levelEnd();
-}
-
-function levelEnd() {
-    dragon.scale += 0.005;
-    dragon.alpha -= 0.005;
-    screenFade.alpha += 0.01;
-
-    if (screenFade.alpha >= 1) {
-        setTimeout(resetGame, 2000);
-    }
-}
-
-if (!gameStarted) {
-    sPerFlap = 90;
-
-    if (gameLoopCounter % 30 === 0 && sPerFlap < 40) {
-        sPerFlap += 2;
-    }
-}
+// ... rest of the code ...
 
 function gameLoop() {
     update();
     draw();
-    if (collisionDetected(dragon, obstacle)) {
-        dragon.collided = true;
-    }
+    // Removed the incorrect call to collisionDetected
+
     if (tapToFly.alpha > 0) {
         tapToFly.alpha -= 0.01;
     }
@@ -196,16 +59,17 @@ function gameLoop() {
     gameLoopCounter++;
 
     if (gameStarted) {
-        if (gameLoopCounter % sPerFlap === 0) {
-            current = (frame.current + 1) % dragonImages.length;
+        if (gameLoopCounter % framesPerFlap === 0) { // Fixed this line
+            frame.current = (frame.current + 1) % dragonImages.length;
         }
     } else {
-        if (gameLoopCounter % framesPerFlap === 0) {
+        if (gameLoopCounter % framesPerFlap === 0) { // Fixed this line
             frame.current = (frame.current + 1) % dragonImages.length;
         }
     }
     requestAnimationFrame(gameLoop);
 }
+   
 gameLoop();
 window.onload = () => {
     setTimeout(() => {
