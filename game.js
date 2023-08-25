c58c82c9c42c43449b01a7d4c125dd2d7eab6c71
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-25 04:00:01 -0500

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ import {
 } from './obstacles.js';
 
 // EndGame
-let endGame = false, gameLoopCounter = 0, endGameTime = 0, obstacleVelocity = 5, obstacleSpawnTime = 4000, lastObstacleTime = Date.now(), topObstacle = true, gameStarted = false, tapToFlyAlpha = 1, bgX = 0, fgX = 0, bgbgX = 0, currentFrame = 0, gameTime = 0, nonTappingFrameCounter = 0, framesPerFlap = 50, dragonFrame = 0, obstacleHeight = 40, imageWidth = canvas.height * 4, dragonStartX = canvas.width * 0.1 - 50, dragonStartY = canvas.height * 0.5, gravity = 0.5, jump = -6;
+let endGame = false, gameLoopCounter = 0, endGameTime = 0, obstacleVelocity = 5, obstacleSpawnTime = 4000, lastObstacleTime = Date.now(), topObstacle = true, gameStarted = false, tapToFlyAlpha = 1, bgX = 0, fgX = 0, bgbgX = 0, currentFrame = 0, gameTime = 0, nonTappingFrameCounter = 0, framesPerFlap = 50, dragonFrame = 0, obstacleHeight = 40, dragonStartX = canvas.width * 0.1 - 50, dragonStartY = canvas.height * 0.5, gravity = 0.5, jump = -6;
 
 // Input handling
 function handleInput() { /* ... */ }
