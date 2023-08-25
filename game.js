e7458815e868b83f9c7883f7497c5a300de216d3
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-25 17:10:41 -0500

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ import {
     createWraithObstacle, createZombieDragonObstacle, createThundercloudObstacle, createFireballObstacle
 } from './obstacles.js';
 
-let obstacleY, spawnRate = 5, spawnTimer = 0, framesPerFlap = 100, gameLoopCounter = 0, gameStarted = false, jump = 8;
+let topObstacle = false, obstacleY, spawnRate = 5, spawnTimer = 0, framesPerFlap = 100, gameLoopCounter = 0, gameStarted = false, jump = 8;
 
 function handleInput() {
     if (!gameStarted) {
