9257ba8c02215dea32818cfbb0c3513da82758c2
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-26 21:59:04 -0500

    Update render.js

--- render.js
+++ render.js
@@ -1,8 +1,7 @@
 import {
   bg,
   backgrounds,
-  dragonImages,
-  frame,
+  dragon,
   obstacles,
   canvas,
   context,
@@ -13,7 +12,6 @@ import {
   perchY,
   perchWidth,
   perchHeight,
-  dragon,
   tapToFly,
   perch,
   lifeBar,
@@ -21,63 +19,52 @@ import {
 } from './init.js';
 
 // Draw objects
-  export function draw() {
-    // Draw the furthest back background (bgbg)
-    context.drawImage(bgbgImage, backgrounds.bgbgX, 0, bg.width, backgrounds.height);
+export function draw() {
+  // Draw the furthest back background (bgbg)
+  context.drawImage(bgbgImage, backgrounds.bgbgX, 0, bg.width, backgrounds.height);
 
-    // Draw the middle background (bg)
-    context.drawImage(bgImage, backgrounds.bgX, 0, bg.width, backgrounds.height);
+  // Draw the middle background (bg)
+  context.drawImage(bgImage, backgrounds.bgX, 0, bg.width, backgrounds.height);
 
-    // Draw the closest background (fg)
-    context.drawImage(fgImage, backgrounds.fgX, 0, bg.width, backgrounds.height);
+  // Draw the closest background (fg)
+  context.drawImage(fgImage, backgrounds.fgX, 0, bg.width, backgrounds.height);
 
-    // Draw perch
-    context.drawImage(perchImage, perch.x, perchY, perchWidth, perchHeight);
+  // Draw perch
+  context.drawImage(perchImage, perch.x, perchY, perchWidth, perchHeight);
 
-    // Drawing obstacles using functions from obstacles.js
-    obstacles.forEach(obstacle => {
-        obstacle.draw(context);
-    });
+  // Drawing obstacles using functions from obstacles.js
+  obstacles.forEach(obstacle => {
+    obstacle.draw(context);
+  });
 
-    // Draw the dragon with scaling and fading
-    context.save();
-    context.globalAlpha = dragon.alpha;
-    context.translate(dragon.x + dragon.width / 2, dragon.y + dragon.height / 2);
-    context.scale(dragon.scale, dragon.scale);
-    context.translate(-(dragon.x + dragon.width / 2), -(dragon.y + dragon.height / 2));
-    context.drawImage(dragonImages[frame.current], dragon.x, dragon.y, dragon.width, dragon.height);
-    context.restore();
+  // Draw the dragon with scaling and fading
+  context.save();
+  context.globalAlpha = dragon.alpha;
+  context.translate(dragon.x + dragon.width / 2, dragon.y + dragon.height / 2);
+  context.scale(dragon.scale, dragon.scale);
+  context.translate(-(dragon.x + dragon.width / 2), -(dragon.y + dragon.height / 2));
+  context.drawImage(dragon.image, dragon.x, dragon.y, dragon.width, dragon.height);  // Modified line
+  context.restore();
 
- // Draw the life bar
-for (let i = 0; i < lifeBar.segments; i++) {
-  // Determine the color of the segment
-  let color = 'green';
-  if (lifeBar.segments <= 2) {
-    color = 'red';
+  // Draw the life bar
+  for (let i = 0; i < lifeBar.segments; i++) {
+    let color = 'green';
+    if (lifeBar.segments <= 2) {
+      color = 'red';
+    }
+    context.fillStyle = color;
+    context.fillRect(10 + i * 35, 10, 35, 30);
   }
 
-  if (dragon.collided) {
-  lifeBar.segments -= 1;
-  context.fillStyle = 'red';
-  context.fillRect(dragon.x - 5, dragon.y - 5, dragon.width + 10, dragon.height + 10);
-}
- // Draw the segment
- context.fillStyle = color;
- context.fillRect(10 + i * 35, 10, 35, 30);
-
-}
   // Draw the "TAP TO FLY!" text
-if (tapToFly.alpha > 0) {
-    context.fillStyle = `rgba(255, 255, 255, ${tapToFly.alpha})`; // White text with alpha for fading
+  if (tapToFly.alpha > 0) {
+    context.fillStyle = `rgba(255, 255, 255, ${tapToFly.alpha})`;
     context.font = '40px sans-serif';
     context.textAlign = 'center';
     context.fillText('TAP TO FLY!', canvas.width / 2, canvas.height / 2);
-}
+  }
 
-
-  
-
-    // Draw black fade overlay
-    context.fillStyle = `rgba(0, 0, 0, ${screenFade.alpha})`;
-    context.fillRect(0, 0, canvas.width, canvas.height);
+  // Draw black fade overlay
+  context.fillStyle = `rgba(0, 0, 0, ${screenFade.alpha})`;
+  context.fillRect(0, 0, canvas.width, canvas.height);
 }
