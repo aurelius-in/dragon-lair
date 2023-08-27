1b1d79d9a961da9dad9667d24fff9f4e07de1da6
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-27 10:17:47 -0500

    Update init.js

--- init.js
+++ init.js
@@ -1,56 +1,77 @@
 // Initialize canvas and context
-let canvas;
-let context;
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
 
-// Initialize other game variables
-let dragon;
-let obstacles;
-let perch;
-let lifeBar;
-let backgrounds;
+// Initialize dragon properties
+const dragon = {
+  x: 50,
+  y: canvas.height / 2,
+  width: 50,
+  height: 50,
+  velocity: 0,
+  image: new Image(),
+  frames: ['frame1.png', 'frame2.png'],
+  alpha: 1,
+  scale: 1,
+  collided: false
+};
 
-export function initializeGame() {
-  canvas = document.getElementById('game');
-  context = canvas.getContext('2d');
+// Initialize perch properties
+const perch = {
+  x: 200,
+  y: canvas.height - 50,
+  width: 50,
+  height: 50,
+  image: new Image()
+};
 
-  // Initialize dragon properties
-  dragon = {
-    x: 50,
-    y: 200,
-    velocity: 0,
-    image: new Image(),
-    frames: ['dragon1.png', 'dragon2.png'],
-    drop: 'dragon_drop.png',
-    collided: false
-  };
+// Initialize obstacles
+const obstacles = [];
 
-  // Initialize obstacles
-  obstacles = [];
+// Initialize life bar
+const lifeBar = {
+  segments: 3,
+  image: new Image()
+};
 
-  // Initialize perch
-  perch = {
-    x: 0,
-    y: 0,
-    image: new Image()
-  };
+// Initialize tap to fly text
+const tapToFly = {
+  alpha: 1
+};
 
-  // Initialize life bar
-  lifeBar = {
-    segments: 3,
-    image: new Image()
-  };
+// Initialize screen fade
+const screenFade = {
+  alpha: 0
+};
 
-  // Initialize backgrounds
-  backgrounds = {
-    fgX: 0,
-    bgX: 0,
-    width: 800,
-    fgImage: new Image(),
-    bgImage: new Image()
-  };
+// Initialize backgrounds
+const backgrounds = {
+  bgImage: new Image(),
+  fgImage: new Image(),
+  bgbgImage: new Image(),
+  bgX: 0,
+  fgX: 0,
+  bgbgX: 0,
+  width: 0,
+  height: 0
+};
+
+// Function to load an image
+export function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+  });
 }
 
-// Getter functions to access variables
+// Initialize the game
+export function initializeGame() {
+  // Load images, set initial positions, etc.
+}
+
+// Getters for other modules to access these variables
 export function getCanvas() {
   return canvas;
 }
@@ -63,16 +84,24 @@ export function getDragon() {
   return dragon;
 }
 
-export function getObstacles() {
-  return obstacles;
-}
-
 export function getPerch() {
   return perch;
 }
 
+export function getObstacles() {
+  return obstacles;
+}
+
 export function getLifeBar() {
   return lifeBar;
+}
+
+export function getTapToFly() {
+  return tapToFly;
+}
+
+export function getScreenFade() {
+  return screenFade;
 }
 
 export function getBackgrounds() {
