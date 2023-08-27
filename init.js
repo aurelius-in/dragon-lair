381a2a6fa9e4a57a8c5ecb3e407e5d522c59fd89
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-27 00:00:14 -0500

    refactoring

--- init.js
+++ init.js
@@ -1,66 +1,80 @@
-// Canvas and context
-export const canvas = document.getElementById('game');
-export const context = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+window.addEventListener('load', function() {
+  // Canvas and context
+  const canvasElement = document.getElementById('game');
+  if (!canvasElement) {
+    console.error('Canvas element not found.');
+    return;
+  }
+  export const canvas = canvasElement;
+  export const context = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-// Images
-export const perchImage = new Image(); perchImage.src = 'images/perch.png';
-export const bgImage = new Image(); bgImage.src = 'images/bg.png';
-export const fgImage = new Image(); fgImage.src = 'images/fg.png';
-export const bgbgImage = new Image(); bgbgImage.src = 'images/bgbg.png';
+  // Function to handle image loading
+  function loadImage(src) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(new Error(`Failed to load image: ${src}`));
+      img.src = src;
+    });
+  }
 
-// Perch
-export const perch = { x: 50, update: function() {} };
-export const perchY = canvas.height - 250;
-export const perchWidth = 150;
-export const perchHeight = 250;
+  // Load images with error handling
+  export const perchImage = await loadImage('images/perch.png').catch(console.error);
+  export const bgImage = await loadImage('images/bg.png').catch(console.error);
+  export const fgImage = await loadImage('images/fg.png').catch(console.error);
+  export const bgbgImage = await loadImage('images/bgbg.png').catch(console.error);
 
-// Dragon
-export const dragon = {
-  collided: false,
-  alpha: 1,
-  scale: 1,
-  x: perch.x,
-  y: perchY - 125,
-  width: 150,
-  height: 150,
-  velocity: 0,
-  image: new Image(),
-  drop: new Image(),
-  start: new Image(),
-  update: function() {},
-  frames: [
-    'images/dragon3.png',
-    'images/dragon4.png',
-    'images/dragon5.png',
-    'images/dragon1.png',
-    'images/dragon2.png',
-    'images/dragon3.png'
-  ]
-};
+  // Perch
+  export const perch = { x: 50, update: function() {} };
+  export const perchY = canvas.height - 250;
+  export const perchWidth = 150;
+  export const perchHeight = 250;
 
-dragon.drop.src = 'images/dragon3.png';
-dragon.start.src = 'images/dragon2.png';
+  // Dragon
+  export const dragon = {
+    collided: false,
+    alpha: 1,
+    scale: 1,
+    x: perch.x,
+    y: perchY - 125,
+    width: 150,
+    height: 150,
+    velocity: 0,
+    image: await loadImage('images/dragon3.png').catch(console.error),
+    drop: await loadImage('images/dragon3.png').catch(console.error),
+    start: await loadImage('images/dragon2.png').catch(console.error),
+    update: function() {},
+    frames: [
+      'images/dragon3.png',
+      'images/dragon4.png',
+      'images/dragon5.png',
+      'images/dragon1.png',
+      'images/dragon2.png',
+      'images/dragon3.png'
+    ]
+  };
 
-// Fading intro text
-export const tapToFly = { alpha: 1 };
+  // Fading intro text
+  export const tapToFly = { alpha: 1 };
 
-// End of game
-export const frame = { current: 0 };
-export const screenFade = { alpha: 0 };
-export const bg = { width: canvas.height * 4 };
+  // End of game
+  export const frame = { current: 0 };
+  export const screenFade = { alpha: 0 };
+  export const bg = { width: canvas.height * 4 };
 
-// Obstacles
-export const obstacles = [];
+  // Obstacles
+  export const obstacles = [];
 
-// Background
-export const backgrounds = {
-  bgbgX: 0,
-  bgX: 0,
-  fgX: 0,
-  height: canvas.height
-};
+  // Background
+  export const backgrounds = {
+    bgbgX: 0,
+    bgX: 0,
+    fgX: 0,
+    height: canvas.height
+  };
 
-// Life Bar
-export const lifeBar = { segments: 10 };
+  // Life Bar
+  export const lifeBar = { segments: 10 };
+});
