e8c7742565054356380cd03cc1c7fe292876e226
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-27 09:03:34 -0500

    Update init.js

--- init.js
+++ init.js
@@ -1,80 +1,84 @@
-window.addEventListener('load', function() {
-  // Canvas and context
+// Internal variables
+let _canvas;
+let _context;
+let _dragon = {
+  collided: false,
+  alpha: 1,
+  scale: 1,
+  x: 50, // Placeholder, will be updated
+  y: 50, // Placeholder, will be updated
+  width: 150,
+  height: 150,
+  velocity: 0,
+  image: null, // Placeholder, will be updated
+  drop: null,  // Placeholder, will be updated
+  start: null, // Placeholder, will be updated
+  frames: [
+    'images/dragon3.png',
+    'images/dragon4.png',
+    'images/dragon5.png',
+    'images/dragon1.png',
+    'images/dragon2.png',
+    'images/dragon3.png'
+  ]
+};
+let _perch = {
+  x: 50,
+  y: 50, // Placeholder, will be updated
+  width: 150,
+  height: 250
+};
+let _perchY = 0; // Placeholder, will be updated
+let _tapToFly = { alpha: 1 };
+let _frame = { current: 0 };
+let _screenFade = { alpha: 0 };
+let _backgrounds = {
+  bgbgX: 0,
+  bgX: 0,
+  fgX: 0,
+  height: 0 // Placeholder, will be updated
+};
+let _lifeBar = { segments: 10 };
+let _obstacles = [];
+
+// Export getter functions
+export function getCanvas() { return _canvas; }
+export function getContext() { return _context; }
+export function getDragon() { return _dragon; }
+export function getPerch() { return _perch; }
+export function getPerchY() { return _perchY; }
+export function getTapToFly() { return _tapToFly; }
+export function getFrame() { return _frame; }
+export function getScreenFade() { return _screenFade; }
+export function getBackgrounds() { return _backgrounds; }
+export function getLifeBar() { return _lifeBar; }
+export function getObstacles() { return _obstacles; }
+
+// Function to load images
+export async function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image: ${src}`));
+    img.src = src;
+  });
+}
+
+// Initialize everything once the window is loaded
+window.addEventListener('load', async function() {
   const canvasElement = document.getElementById('game');
   if (!canvasElement) {
     console.error('Canvas element not found.');
     return;
   }
-  export const canvas = canvasElement;
-  export const context = canvas.getContext('2d');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  _canvas = canvasElement;
+  _context = _canvas.getContext('2d');
+  _canvas.width = window.innerWidth;
+  _canvas.height = window.innerHeight;
 
-  // Function to handle image loading
-  function loadImage(src) {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.onload = () => resolve(img);
-      img.onerror = () => reject(new Error(`Failed to load image: ${src}`));
-      img.src = src;
-    });
-  }
-
-  // Load images with error handling
-  export const perchImage = await loadImage('images/perch.png').catch(console.error);
-  export const bgImage = await loadImage('images/bg.png').catch(console.error);
-  export const fgImage = await loadImage('images/fg.png').catch(console.error);
-  export const bgbgImage = await loadImage('images/bgbg.png').catch(console.error);
-
-  // Perch
-  export const perch = { x: 50, update: function() {} };
-  export const perchY = canvas.height - 250;
-  export const perchWidth = 150;
-  export const perchHeight = 250;
-
-  // Dragon
-  export const dragon = {
-    collided: false,
-    alpha: 1,
-    scale: 1,
-    x: perch.x,
-    y: perchY - 125,
-    width: 150,
-    height: 150,
-    velocity: 0,
-    image: await loadImage('images/dragon3.png').catch(console.error),
-    drop: await loadImage('images/dragon3.png').catch(console.error),
-    start: await loadImage('images/dragon2.png').catch(console.error),
-    update: function() {},
-    frames: [
-      'images/dragon3.png',
-      'images/dragon4.png',
-      'images/dragon5.png',
-      'images/dragon1.png',
-      'images/dragon2.png',
-      'images/dragon3.png'
-    ]
-  };
-
-  // Fading intro text
-  export const tapToFly = { alpha: 1 };
-
-  // End of game
-  export const frame = { current: 0 };
-  export const screenFade = { alpha: 0 };
-  export const bg = { width: canvas.height * 4 };
-
-  // Obstacles
-  export const obstacles = [];
-
-  // Background
-  export const backgrounds = {
-    bgbgX: 0,
-    bgX: 0,
-    fgX: 0,
-    height: canvas.height
-  };
-
-  // Life Bar
-  export const lifeBar = { segments: 10 };
+  _dragon.image = await loadImage('images/dragon3.png').catch(console.error);
+  _dragon.drop = await loadImage('images/dragon3.png').catch(console.error);
+  _dragon.start = await loadImage('images/dragon2.png').catch(console.error);
+  _perchY = _canvas.height - 250;
+  _backgrounds.height = _canvas.height;
 });
